48482a99cbe6319fc439ef198290fd5e8ebeb245
BenHaddou El-Mehdi (EXTERNAL) <EM.BenHaddou@hq.eapl.europcar.vwfs-ad>
2018-01-29 17:40:32 +0100

    init project

---
+++ src/main/java/com/skillm/skillclassification/Application.java
@@ -0,0 +1,13 @@
+package com.skillm.skillclassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
